7a5625ec7895eb160ce9cfe3ba6571dcea5a9d37
jose-poveda <jose.poveda@correo.uady.mx>
2014-09-17 00:39:45 -0500

    Created a tidy data set

---
+++ Readme.md
@@ -0,0 +1,33 @@
+#Course Project for Getting and Cleaning Data
+##Readme for run_analysis.R
+###Please read this file before opening it
+
+You need these R packages in order to run this program:
+*dplyr
+*tidyr
+*reshape2
+
+You need to download the file from this URL:
+[adress]https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip[id]
+
+This zip file __must be unzipped in your working directory__, using the option
+_"extract here"_ to ensure that the original folder won't get duplicated.
+You don't need to modify any name file or name folder, it will work with their
+default names.
+
+The `run_analysis.R` runs automatically from this point. This file will:
+*Read `X_train.txt`, `X_test.txt`, and bind them together.
+*Read `features.txt` and choose only the mean() and std() variables.
+*Read `subject_train.txt`, `subject_test.txt`, and bind them together.
+*Read `y_train.txt`, `y_test.txt` and bind them together.
+*Modify the names of Variables and Activities.
+*Bind all the pieces together.
+*Readying up the variable names for tidying. This included:
+  +Renaming the variables with recognizable names.
+  +Renaming the values with easy to identify names.
+  +Changing the character values to integer values as noted in the Codebook.
+*Tidying up the variables with tidyr. This included:
+  +Gathering all the variables into a column
+  +Separating all the variables back into their respective column.
+*Average every variable by ID and Activity
+*Write a file called `tidy_data.txt` in the working directory with the last data set.
---
+++ run_analysis.R
@@ -0,0 +1,95 @@
+train <- read.table("./UCI HAR Dataset/train/X_train.txt", sep = "")
+test <- read.table("./UCI HAR Dataset/test/X_test.txt", sep = "")
+allt <- rbind(train, test)
+#Now we read the features text file and locate the "mean" and "std" variables
+features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+i <- grep("mean\\(\\)|std", features$V2)
+library(dplyr)
+#Subsetting and changing the variable names
+names(allt) <- features$V2
+suball <- select(allt, i)
+#Adding the identifiers for the subjects and the different activities
+subjecttrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", sep = "")
+subjecttest <- read.table("./UCI HAR Dataset/test/subject_test.txt", sep = "")
+subject <- rbind(subjecttrain, subjecttest)
+names(subject) <- "SubjectID"
+activitytrain <- read.table("./UCI HAR Dataset/train/y_train.txt", sep = "")
+activitytest <- read.table("./UCI HAR Dataset/test/y_test.txt", sep = "")
+activity <- rbind(activitytrain, activitytest)
+names(activity) <- "Activities"
+#Binding Activities and ID's together
+subjact <- cbind(subject, activity)
+#Using names easier to search for
+labels <- read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
+subjact$Activities <- gsub("1", labels[1, 2], subjact$Activities)
+subjact$Activities <- gsub("2", labels[2, 2], subjact$Activities)
+subjact$Activities <- gsub("3", labels[3, 2], subjact$Activities)
+subjact$Activities <- gsub("4", labels[4, 2], subjact$Activities)
+subjact$Activities <- gsub("5", labels[5, 2], subjact$Activities)
+subjact$Activities <- gsub("6", labels[6, 2], subjact$Activities)
+suball <- cbind(subjact, suball)
+#Now it's time to change the variable names with easier to identify names.
+names(suball) <- gsub("\\(\\)", "", names(suball))
+names(suball) <- gsub("^t", "Time", names(suball))
+names(suball) <- gsub("^f", "Frequency", names(suball))
+names(suball) <- gsub("Acc", "Accelerometer", names(suball))
+names(suball) <- gsub("Gyro", "Gyroscope", names(suball))
+names(suball) <- gsub("Mag", "Magnitude", names(suball))
+names(suball) <- gsub("(Body){2}", "Body", names(suball))
+names(suball) <- gsub("-std", "Standarddeviation", names(suball))
+names(suball) <- gsub("-mean", "Mean", names(suball))
+names(suball) <- gsub("-", "", names(suball))
+#Creating a new data set to perform the tidying
+subtidy <- suball
+library(tidyr)
+#Doing tidying, first, readying up the names to use tidyr
+names(subtidy) <- gsub("Time", "Time_", names(subtidy))
+names(subtidy) <- gsub("Frequency", "Frequency_", names(subtidy))
+names(subtidy) <- gsub("Body", "Body_", names(subtidy))
+names(subtidy) <- gsub("Gravity", "Gravity_", names(subtidy))
+names(subtidy) <- gsub("Accelerometer", "Accelerometer_", names(subtidy))
+names(subtidy) <- gsub("Gyroscope", "Gyroscope_", names(subtidy))
+names(subtidy) <- gsub("Mean", "Mean_", names(subtidy))
+names(subtidy) <- gsub("Standarddeviation", "Standarddeviation_", names(subtidy))
+names(subtidy) <- gsub("Jerk", "Jerk_", names(subtidy))
+names(subtidy) <- gsub("Magnitude", "Magnitude_", names(subtidy))
+names(subtidy) <- gsub("_Mean_X", "_X_Mean", names(subtidy))
+names(subtidy) <- gsub("_Mean_Y", "_Y_Mean", names(subtidy))
+names(subtidy) <- gsub("_Mean_Z", "_Z_Mean", names(subtidy))
+names(subtidy) <- gsub("_Standarddeviation_X", "_X_Standarddeviation", names(subtidy))
+names(subtidy) <- gsub("_Standarddeviation_Y", "_Y_Standarddeviation", names(subtidy))
+names(subtidy) <- gsub("_Standarddeviation_Z", "_Z_Standarddeviation", names(subtidy))
+names(subtidy) <- gsub("\\_$", "", names(subtidy))
+names(subtidy) <- gsub("e_X", "e_No_X", names(subtidy))
+names(subtidy) <- gsub("e_Y", "e_No_Y", names(subtidy))
+names(subtidy) <- gsub("e_Z", "e_No_Z", names(subtidy))
+names(subtidy) <- gsub("r_X", "r_No_X", names(subtidy))
+names(subtidy) <- gsub("r_Y", "r_No_Y", names(subtidy))
+names(subtidy) <- gsub("r_Z", "r_No_Z", names(subtidy))
+names(subtidy) <- gsub("e_Ma", "e_No_Ma", names(subtidy))
+names(subtidy) <- gsub("r_Ma", "r_No_Ma", names(subtidy))
+names(subtidy) <- gsub("Jerk", "Yes", names(subtidy))
+subtidynum <- subtidy
+names(subtidynum) <- gsub("No", "0", names(subtidynum))
+names(subtidynum) <- gsub("Yes|Time|Accelerometer|X|Mean|Body", "1", names(subtidynum))
+names(subtidynum) <- gsub("Frequency|Gyroscope|Y|Standarddeviation|Gravity", "2", names(subtidynum))
+names(subtidynum) <- gsub("Z", "3", names(subtidynum))
+names(subtidynum) <- gsub("Magnitude", "4", names(subtidynum))
+#Making sure there are 6 variables in the names, first two don't need to have "_"
+grep("(.*\\_){5}", names(subtidynum))
+#Using tidyr to put variables as columns
+subtidynum <- gather(subtidynum, domain, Measure, -SubjectID, -Activities)
+subtidynum <- separate(subtidynum, domain, c("Domain", "Acceleration", "Device", "Jerk", "Axis", "Statistic"))
+#Making sure numbers are integers so we're able to find the mean
+subtidynum$Domain <- as.integer(subtidynum$Domain)
+subtidynum$Acceleration <- as.integer(subtidynum$Acceleration)
+subtidynum$Device <- as.integer(subtidynum$Device)
+subtidynum$Jerk <- as.integer(subtidynum$Jerk)
+subtidynum$Axis <- as.integer(subtidynum$Axis)
+subtidynum$Statistic <- as.integer(subtidynum$Statistic)
+#Summarizing the info doing reshaping.
+library(reshape2)
+meltsubtidynum <- melt(subtidynum, id.vars = c("SubjectID", "Activities"), na.rm = TRUE)
+sumtidynum <- dcast(meltsubtidynum, SubjectID + Activities ~ variable, mean)
+#Now we have to write this table so everyone is able to read it
+write.table(sumtidynum, "tidy_data.txt", row.names = FALSE)
